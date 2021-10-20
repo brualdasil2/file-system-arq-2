6ca89f793751748cea229d6038f0d9092788bc1a
Tiago Torres Schmidt <tiago.schmidt@inf.ufrgs.br>
2021-10-20 11:01:05 -0300

    Correção de main.

--- main.c
+++ main.c
@@ -3,33 +3,5 @@
 #include "fs.h"
 
 int main() {
-    FS fileSystem = initFS();
-    const char spc[2] = " ";
-    char input[210];
-    char *cmd,*path;
-    /*while (1) {
-        printf("%s> ", fileSystem.dirState.workingDir);
-        fgets(input, sizeof(input), stdin);
-        input[strlen(input)-1] = '\0'; //Retira o '/n' do final
-
-        cmd = strtok(input,spc);
-        path = strtok(NULL,spc);
-
-        if(!strcmp("CD",cmd)) cd(path, &fileSystem);
-        else if(!strcmp("RM",cmd)) rm(path, &fileSystem);
-        else if(!strcmp("MOVE",cmd)) move(path, strtok(NULL,spc),&fileSystem);
-        else if(!strcmp("EXIT",cmd)) {
-            fclose(fileSystem.arquivo);
-            //TODO: definir exit(), a qual libera os espaços previamente alocados pela initFS()
-            return 0;
-        }
-        else printf("Comando desconhecido.\n");       
-    }*/
-    /*make("dir1","dir",fileSystem);
-    make("/root/dir1/dir2","txt",fileSystem);
-    make("/root/dir1/tiago","txt",fileSystem);
-    dir(fileSystem);  */
-    renameFile("/root/dir1/tiago.txt","dir2",fileSystem);
-    renameFile("/root/dir1/tiago.txt","teste",fileSystem);
-    //dir(fileSystem);
+    
 }
